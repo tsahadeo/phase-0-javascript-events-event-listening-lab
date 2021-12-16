7a95c87a7857d4af4ffcb51435931a381260314b
Travis <tsahadeo@mac.com>
2021-12-16 12:58:33 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function addingEventListener (click, clickAlert) {
-    
-    function clickAlert() {
-        alert ('I was clicked!')
-    }
-    input.addEventListener('click', clickAlert);
+function addingEventListener() {
+const input = document.getElementById('input');
+
+function clickAlert() {
+alert('I was clicked!');
 }
+input.addEventListener('click', clickAlert);
+}
+
+addingEventListener()
